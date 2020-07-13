0072a5083673b5597d523711274047e07761f674
박수봉 <dexter@urbanbase.com>
2020-07-13 09:48:05 +0900

    create project

---
+++ src/background.js
@@ -0,0 +1,9 @@
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+// import store from './store'
+//
+// alert(`Hello ${store.getters.foo}!`)
+
---
+++ src/popup/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="Lupin">
+    <div class="header">
+      <div class="header-name-container">
+        <h2>닉네임</h2>
+      </div>
+      <div class="header-button-container">
+        <i class="fas fa-cog"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userName : this.$store.getters.foo
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
